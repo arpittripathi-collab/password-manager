7a81b9d53267028918e49e11c87470f34b131fa0
Arpit Tripathi <144149689+arpittripathi-collab@users.noreply.github.com>
2025-04-25 15:04:31 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 
 
 
-app.use(cors({credentials: true, origin: 'password-manager-cfqsi30n8-arpit-tripathis-projects-f24fb41b.vercel.app'}));
+app.use(cors({credentials: true, origin: 'https://password-manager-six-lemon.vercel.app/'}));
 app.use(cookieParser());   
 
 // SETTING UP DOTENV

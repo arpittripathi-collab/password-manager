16687dabac45c94a0a5bd5bc29000f07f2fbcaf7
Arpit Tripathi <arpit.tripathi0610@gmail.com>
2025-04-25 14:01:09 +0530

    testing render beckend

--- client/src/axios/instance.js
+++ client/src/axios/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const isDev = true;
 
-const url = isDev ? "http://localhost:8000" : "";
+const url = isDev ? "https://password-manager-rsi7.onrender.com" : "";
 
 const instance = axios.create({
     baseURL: url,

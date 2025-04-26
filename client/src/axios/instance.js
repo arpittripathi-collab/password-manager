4595931cb0fc171a3f57f18bcc981bf818f77003
Arpit Tripathi <144149689+arpittripathi-collab@users.noreply.github.com>
2025-04-26 10:26:37 +0530

    Update instance.js

--- client/src/axios/instance.js
+++ client/src/axios/instance.js
@@ -1,29 +1,21 @@
 import axios from "axios";
+const isDev = true;
 
-const API_BASE =
-  process.env.REACT_APP_API_URL ||
-  "https://password-manager-backend-n6jx.onrender.com";
+const url = isDev ? "https://password-manager-rsi7.onrender.com" : "";
 
 const instance = axios.create({
-  baseURL: API_BASE,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  },
-  withCredentials: true
+    baseURL: url,
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    },
+    withCredentials: true
 });
 
-// AUTH
-export const loginUser       = (data) => instance.post("/auth/login", data);
-export const signupUser      = (data) => instance.post("/auth/register", data);
-export const logoutUser      = ()    => instance.post("/auth/logout");
-
-// USER
-export const checkAuthenticated = ()        => instance.get("/api/authenticate");
-
-// VAULT
-export const saveNewPassword = (data)       => instance.post("/api/vault", data);
-export const deleteAPassword = (id)         => instance.delete(`/api/vault/${id}`);
-
-// DECRYPT
-export const decryptThePass   = (data)      => instance.post("/api/decrypt", data);
+export const checkAuthenticated = () => instance.get("/authenticate");
+export const loginUser = (data) => instance.post("/login", data); 
+export const logoutUser = () => instance.get("/logout");
+export const signupUser = (data) => instance.post("/register", data);
+export const saveNewPassword = (data) => instance.post("/addnewpassword", data);
+export const deleteAPassword = (id) => instance.post("/deletepassword", id);
+export const decryptThePass = (data) => instance.post("/decrypt", data);

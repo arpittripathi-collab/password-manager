ad8eb5984091a24dda6943e2093b011174b7af30
Arpit Tripathi <144149689+arpittripathi-collab@users.noreply.github.com>
2025-04-28 01:13:06 +0530

    Update Home.js

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import "./Home.css";
 
 function Home() {
-    const { name, isAuthenticated } = useSelector(state => state);
+    const isAuthenticated = useSelector(state => state.isAuthenticated);
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        const storedName = localStorage.getItem("userName");
+        if (storedName) {
+            setName(storedName);
+        }
+    }, []);
 
     return (
         <div className="home">
             <div className="home__left">
                 {!isAuthenticated ? (
                     <>
-                        <h1 >
+                        <h1>
                             Welcome to <span className="name">Password Manager</span>
                         </h1>
-                        <p >
+                        <p>
                             The best and most secure way to store your passwords, all in one place.
                         </p>
-                        <Link to="/signup" >
+                        <Link to="/signup">
                             <i className="fas fa-user-plus"></i> Sign Up Now
                         </Link>
                     </>
                 ) : (
                     <>
-                        <h1  >
+                        <h1>
                             Welcome, <span className="name">{name}</span>
                         </h1>
                         <p>
